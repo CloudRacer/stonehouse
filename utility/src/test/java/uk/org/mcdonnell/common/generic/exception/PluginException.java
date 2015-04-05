bae50178df62a1dcae41e22f55254a18eabe432e
John McDonnell <jmcdonnell3@csc.com>
2015-04-05 23:21:36 +0100

    Generic routines to ad a JAR file to the classpath at runtime.

---
+++ utility/src/test/java/uk/org/mcdonnell/common/generic/exception/PluginException.java
@@ -0,0 +1,34 @@
+package uk.org.mcdonnell.common.generic.exception;
+
+public class PluginException extends Exception
+{
+
+    /**
+	 * 
+	 */
+    private static final long serialVersionUID = 1L;
+
+    public PluginException()
+    {
+
+    }
+
+    public PluginException(String arg0)
+    {
+        super(arg0);
+
+    }
+
+    public PluginException(Throwable arg0)
+    {
+        super(arg0);
+
+    }
+
+    public PluginException(String arg0, Throwable arg1)
+    {
+        super(arg0, arg1);
+
+    }
+
+}
