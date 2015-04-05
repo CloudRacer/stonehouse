08389a6d35b7a139938c33d2b62542c21217dc61
John McDonnell <jmcdonnell3@csc.com>
2015-04-05 15:17:25 +0100

    Handle exceptions from the component that is being tested.

--- Provider/src/test/java/uk/org/mcdonnell/stonehouse/service/ProviderConnectionTest.java
+++ Provider/src/test/java/uk/org/mcdonnell/stonehouse/service/ProviderConnectionTest.java
@@ -2,6 +2,7 @@ package uk.org.mcdonnell.stonehouse.service;
 
 import javax.naming.InitialContext;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -11,7 +12,7 @@ import junitx.util.PrivateAccessor;
 public class ProviderConnectionTest {
 
 	@Test
-	public void testProviderConnection() throws Throwable {
+	public void testProviderConnection() {
 		final String PROTOCOL = "t3";
 		final String SERVER_NAME = "localhost";
 		final int PORT = 7001;
@@ -22,12 +23,22 @@ public class ProviderConnectionTest {
 		final String JMS_SERVER_NAME = "WM6JMSServer";
 		final String JMS_MODULE_NAME = "WM6_QUEUES";
 
-		InitialContext initialContext = (InitialContext) PrivateAccessor.invoke(ProviderConnection.class, "getInitialContext",
-				new Class[] { String.class, String.class, String.class }, new Object[] { URL, USERNAME, PASSWORD });
+		InitialContext initialContext;
+		try {
+			initialContext = (InitialContext) PrivateAccessor.invoke(
+					ProviderConnection.class, "getInitialContext", new Class[] {
+							String.class, String.class, String.class },
+					new Object[] { URL, USERNAME, PASSWORD });
 
-		assertTrue(initialContext.getEnvironment().containsKey("java.naming.provider.url"));
-		assertEquals(initialContext.getEnvironment().get("java.naming.provider.url"), URL);
-		
-		ProviderConnection providerConnection = new ProviderConnection();
+			assertTrue(initialContext.getEnvironment().containsKey(
+					"java.naming.provider.url"));
+			assertEquals(
+					initialContext.getEnvironment().get(
+							"java.naming.provider.url"), URL);
+
+			ProviderConnection providerConnection = new ProviderConnection();
+		} catch (Throwable e) {
+			Assert.fail("Error occurred: " + e.getMessage());
+		}
 	}
 }
