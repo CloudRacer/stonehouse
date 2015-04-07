9333f1deff0545edc22468a8440fa60c3b52bec1
weblogic <john@mcdonnell.org.uk>
2015-04-07 16:04:09 +0100

    Refactored with a new name.

---
+++ application/src/main/java/uk/org/mcdonnell/stonehouse/Application.java
@@ -0,0 +1,14 @@
+package uk.org.mcdonnell.stonehouse;
+
+/**
+ * Hello world!
+ * 
+ */
+public class Application
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
