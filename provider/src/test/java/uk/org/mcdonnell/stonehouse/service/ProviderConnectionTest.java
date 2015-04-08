42eef722e64f6c6a17a67ec3ec10a850acf05a29
weblogic <john@mcdonnell.org.uk>
2015-04-09 00:00:41 +0100

    Tests run against only one server.

--- provider/src/test/java/uk/org/mcdonnell/stonehouse/service/ProviderConnectionTest.java
+++ provider/src/test/java/uk/org/mcdonnell/stonehouse/service/ProviderConnectionTest.java
@@ -1,63 +1,59 @@
 package uk.org.mcdonnell.stonehouse.service;
 
+import static org.junit.Assert.assertTrue;
+
 import java.util.Hashtable;
+
 import javax.naming.Context;
+
+import junitx.util.PrivateAccessor;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 import uk.org.mcdonnell.common.generic.PropertyManipulation;
-import static org.junit.Assert.assertTrue;
-import junitx.util.PrivateAccessor;
 
 public class ProviderConnectionTest {
 
-	@SuppressWarnings("unchecked")
-	@Test
-	public void testProviderJNDIInitialContextEnvironmentIsEmpty() {
-		Hashtable<String, String> emptyJNDIInitialContextEnvironment;
-		try {
-			ProviderConnection providerConnection = new ProviderConnection();
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testProviderJNDIInitialContextEnvironmentIsEmpty() {
+        Hashtable<String, String> emptyJNDIInitialContextEnvironment;
+        try {
+            ProviderConnection providerConnection = new ProviderConnection();
 
-			emptyJNDIInitialContextEnvironment = (Hashtable<String, String>) PrivateAccessor
-					.invoke(providerConnection,
-							"getJNDIInitialContextEnvironment", new Class[] {},
-							new Object[] {});
+            emptyJNDIInitialContextEnvironment = (Hashtable<String, String>) PrivateAccessor
+                    .invoke(providerConnection,
+                            "getJNDIInitialContextEnvironment", new Class[] {},
+                            new Object[] {});
 
-			assertTrue(emptyJNDIInitialContextEnvironment != null
-					&& emptyJNDIInitialContextEnvironment.isEmpty());
-		} catch (Throwable e) {
-			Assert.fail("Error occurred: " + e.getMessage());
-		}
-	}
+            assertTrue(emptyJNDIInitialContextEnvironment != null
+                    && emptyJNDIInitialContextEnvironment.isEmpty());
+        } catch (Throwable e) {
+            Assert.fail("Error occurred: " + e.getMessage());
+        }
+    }
 
-	@Test
-	public void testGetAllProviders() throws Throwable {
-		ProviderConnectionFactory providerConnectionFactory = new ProviderConnectionFactory();
-		PropertyManipulation propertyManipulation;
-		providerConnectionFactory.getAllProviders();
+    @Test
+    public void testGetAllProviders() throws Throwable {
+        ProviderConnectionFactory providerConnectionFactory = new ProviderConnectionFactory();
+        PropertyManipulation propertyManipulation;
+        providerConnectionFactory.getAllProviders();
 
-		assertTrue(providerConnectionFactory.getAllProviders() != null);
-		assertTrue(!providerConnectionFactory.getAllProviders().isEmpty());
+        assertTrue(providerConnectionFactory.getAllProviders() != null);
+        assertTrue(!providerConnectionFactory.getAllProviders().isEmpty());
 
-		propertyManipulation = (PropertyManipulation) PrivateAccessor.invoke(
-				providerConnectionFactory, "getPropertyManipulation",
-				new Class[] {}, new Object[] {});
+        propertyManipulation = (PropertyManipulation) PrivateAccessor.invoke(
+                providerConnectionFactory, "getPropertyManipulation",
+                new Class[] {}, new Object[] {});
 
-		assertTrue(propertyManipulation.containsKey(String.format("1.%s",
-				Context.INITIAL_CONTEXT_FACTORY)));
-		assertTrue(propertyManipulation.containsKey(String.format("1.%s",
-				Context.PROVIDER_URL)));
-		assertTrue(propertyManipulation.containsKey(String.format("1.%s",
-				Context.SECURITY_PRINCIPAL)));
-		assertTrue(propertyManipulation.containsKey(String.format("1.%s",
-				Context.SECURITY_CREDENTIALS)));
-		assertTrue(propertyManipulation.containsKey(String.format("2.%s",
-				Context.INITIAL_CONTEXT_FACTORY)));
-		assertTrue(propertyManipulation.containsKey(String.format("2.%s",
-				Context.PROVIDER_URL)));
-		assertTrue(propertyManipulation.containsKey(String.format("2.%s",
-				Context.SECURITY_PRINCIPAL)));
-		assertTrue(propertyManipulation.containsKey(String.format("2.%s",
-				Context.SECURITY_CREDENTIALS)));
-	}
+        assertTrue(propertyManipulation.containsKey(String.format("1.%s",
+                Context.INITIAL_CONTEXT_FACTORY)));
+        assertTrue(propertyManipulation.containsKey(String.format("1.%s",
+                Context.PROVIDER_URL)));
+        assertTrue(propertyManipulation.containsKey(String.format("1.%s",
+                Context.SECURITY_PRINCIPAL)));
+        assertTrue(propertyManipulation.containsKey(String.format("1.%s",
+                Context.SECURITY_CREDENTIALS)));
+    }
 }
