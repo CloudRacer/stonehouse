c41f110faf311f1b6b60a12a91bec860e4dd473b
weblogic <john@mcdonnell.org.uk>
2015-04-10 16:17:46 +0100

    Updated test to include a ProviderConnection on the Destination.

--- provider/src/test/java/uk/org/mcdonnell/stonehouse/service/DestinationTest.java
+++ provider/src/test/java/uk/org/mcdonnell/stonehouse/service/DestinationTest.java
@@ -2,6 +2,12 @@ package uk.org.mcdonnell.stonehouse.service;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+import java.util.InvalidPropertiesFormatException;
+
+import javax.jms.JMSException;
+import javax.naming.NamingException;
+
 import org.junit.Test;
 
 import uk.org.mcdonnell.stonehouse.service.Destinations.DestinationType;
@@ -9,11 +15,17 @@ import uk.org.mcdonnell.stonehouse.service.Destinations.DestinationType;
 public class DestinationTest {
 
     @Test
-    public void getDetination() {
+    public void getDetination() throws InvalidPropertiesFormatException, IOException, NamingException, JMSException {
         final String TEST_QUEUE_NAME = "Conv10AMSendQueue";
 
-        Destination destination = new Destination(DestinationType.QUEUE, TEST_QUEUE_NAME);
+        ProviderConnectionFactory providerConnectionFactory = new ProviderConnectionFactory();
+        ProviderConnection providerConnection = providerConnectionFactory.getAllProviders().get(0);
+        Destination destination = new Destination(providerConnection, DestinationType.QUEUE, TEST_QUEUE_NAME);
 
         assertTrue(destination.getDestinationName() == TEST_QUEUE_NAME);
+
+        destination.getTotalNumberOfPendingMessages();
+
+        assertTrue(destination.getTotalNumberOfPendingMessages() == 0);
     }
 }
