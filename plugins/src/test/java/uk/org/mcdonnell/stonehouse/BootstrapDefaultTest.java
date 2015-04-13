8e1d2447b2d988c29039f089960c17f7facca000
weblogic <john@mcdonnell.org.uk>
2015-04-14 00:29:55 +0100

    Rename to refactor.

---
+++ plugins/src/test/java/uk/org/mcdonnell/stonehouse/BootstrapDefaultTest.java
@@ -0,0 +1,21 @@
+package uk.org.mcdonnell.stonehouse;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class BootstrapDefaultTest {
+
+    @Test
+    public void ClasspathInitialisationTest() {
+        try {
+            @SuppressWarnings("unused")
+            Bootstrap bootstrap = new Bootstrap();
+
+            assertTrue(true);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+}
