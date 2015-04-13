1b9a773e820a9664032eba59993c2c2f55bf6e3d
weblogic <john@mcdonnell.org.uk>
2015-04-13 23:43:40 +0100

    Corrected the list of classes.

--- plugins/src/test/java/uk/org/mcdonnell/stonehouse/PluginsTest.java
+++ plugins/src/test/java/uk/org/mcdonnell/stonehouse/PluginsTest.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ PluginsTest.class })
+@Suite.SuiteClasses({ BootstrapTest.class })
 public class PluginsTest
 {
 }
