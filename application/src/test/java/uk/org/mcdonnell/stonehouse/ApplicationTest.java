9cacf015f9f8776da6cb8ea2a3fa8a5afd221082
weblogic <john@mcdonnell.org.uk>
2015-04-14 00:41:40 +0100

    Removed unnecessary specialisation.

--- application/src/test/java/uk/org/mcdonnell/stonehouse/ApplicationTest.java
+++ application/src/test/java/uk/org/mcdonnell/stonehouse/ApplicationTest.java
@@ -1,13 +1,10 @@
 package uk.org.mcdonnell.stonehouse;
 
-import junit.framework.TestCase;
-
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({ QueueTest.class })
 public class ApplicationTest
-        extends TestCase
 {
 }
