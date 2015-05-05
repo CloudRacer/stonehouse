815f1a197a73d1d5d4e8fc60648a900baa238c46
weblogic <john@mcdonnell.org.uk>
2015-05-05 13:27:16 +0100

    Refactoring

--- application/src/test/java/uk/org/mcdonnell/stonehouse/ApplicationTest.java
+++ application/src/test/java/uk/org/mcdonnell/stonehouse/ApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ QueueTest.class })
+@Suite.SuiteClasses({ BootstrapDefaultTest.class, QueueTest.class })
 public class ApplicationTest
 {
 }
--- application/src/test/java/uk/org/mcdonnell/stonehouse/QueueTest.java
+++ application/src/test/java/uk/org/mcdonnell/stonehouse/QueueTest.java
@@ -1,17 +1,25 @@
 package uk.org.mcdonnell.stonehouse;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.MalformedURLException;
 import java.util.InvalidPropertiesFormatException;
 import java.util.List;
 
 import javax.naming.NamingException;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import uk.org.mcdonnell.stonehouse.api.connection.ProviderConnection;
 import uk.org.mcdonnell.stonehouse.api.connection.ProviderConnectionFactory;
 
 public class QueueTest {
+
+    @BeforeClass
+    public static void setupClass() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, MalformedURLException, IOException {
+        new Bootstrap();
+    }
 
     @Test
     public void testGetAllQueues() throws InvalidPropertiesFormatException, IOException, NamingException {
