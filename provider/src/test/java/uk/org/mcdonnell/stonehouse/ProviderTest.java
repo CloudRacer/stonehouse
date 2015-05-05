850a65a6c23de0b24de8ff20db7441c032c93c6b
weblogic <john@mcdonnell.org.uk>
2015-05-05 14:25:55 +0100

    Initialise an Apache ActiveMQ broker that can now be used for testing.

--- provider/src/test/java/uk/org/mcdonnell/stonehouse/ProviderTest.java
+++ provider/src/test/java/uk/org/mcdonnell/stonehouse/ProviderTest.java
@@ -1,10 +1,12 @@
 package uk.org.mcdonnell.stonehouse;
 
 import java.io.File;
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.net.MalformedURLException;
 
+import org.apache.activemq.broker.BrokerPlugin;
+import org.apache.activemq.broker.BrokerService;
+import org.apache.activemq.network.NetworkConnector;
+import org.apache.activemq.security.JaasAuthenticationPlugin;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -17,9 +19,47 @@ import uk.org.mcdonnell.stonehouse.service.ProviderConnectionTest;
 @Suite.SuiteClasses({ ProviderConnectionTest.class, DestinationsTest.class, DestinationTest.class })
 public class ProviderTest {
     private final static File DEFAULT_FOLDER = new File("../application/plugins");
+    private static BrokerService broker;
 
     @BeforeClass
-    public static void setupClass() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, MalformedURLException, IOException {
+    public static void setupClass() throws Exception {
         new Bootstrap(DEFAULT_FOLDER);
+
+        // Start the ActiveMQ broker.
+        getBroker();
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        setBroker(null);
+    }
+
+    private static BrokerService getBroker() throws Exception {
+        if (broker == null) {
+            broker = new BrokerService();
+            broker.setBrokerName("stonehouse");
+            broker.setUseShutdownHook(false);
+            broker.setPersistent(false);
+            broker.setUseJmx(false);
+            // Add plugin
+            broker.setPlugins(new BrokerPlugin[] { new JaasAuthenticationPlugin() });
+            // Add a network connection
+            final NetworkConnector connector = broker.addNetworkConnector("static://" + "tcp://somehost:61616");
+            connector.setDuplex(true);
+            broker.addConnector("tcp://localhost:61616");
+            broker.start();
+        }
+
+        return broker;
+    }
+
+    private static void setBroker(BrokerService broker) throws Exception {
+        if (broker == null) {
+            // connector.stop();
+            // connection.close();
+            ProviderTest.broker.stop();
+        }
+
+        ProviderTest.broker = broker;
     }
 }
