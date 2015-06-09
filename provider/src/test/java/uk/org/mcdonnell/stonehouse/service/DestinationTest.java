a9fc6ace5339f5da8965209668d7da6daa4cbfa1
weblogic <john@mcdonnell.org.uk>
2015-06-09 20:37:46 +0100

    AMQ test data set.

--- provider/src/test/java/uk/org/mcdonnell/stonehouse/service/DestinationTest.java
+++ provider/src/test/java/uk/org/mcdonnell/stonehouse/service/DestinationTest.java
@@ -21,15 +21,13 @@ public class DestinationTest {
 
     @Test
     public void getSingleDestination() throws InvalidPropertiesFormatException, IOException, NamingException, JMSException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, ClassNotFoundException, InstantiationException, DestinationStatisticsFactoryUnsupportedException {
-        final String TEST_QUEUE_NAME = "Crane31AMProcessQueue";
+        final String TEST_QUEUE_NAME = "test_queue_1";
 
-        final ProviderConnections providerConnectionFactory = new ProviderConnections();
-        final ProviderConnectionFactory providerConnection = providerConnectionFactory.getAllProviders().get(0);
-        final Destination destination = new Destination(providerConnection, DestinationType.QUEUE, TEST_QUEUE_NAME);
+        final ProviderConnections providerConnections = new ProviderConnections();
+        final ProviderConnectionFactory providerConnectionFactory = providerConnections.getAllProviders().get(1);
+        final Destination destination = new Destination(providerConnectionFactory, DestinationType.QUEUE, TEST_QUEUE_NAME);
 
         assertTrue(destination.getDestinationName() == TEST_QUEUE_NAME);
         assertTrue(destination.getPending() == 0);
-
-        System.out.println(String.format("%s: %s.", destination.getDestinationName(), destination.getPending()));
     }
 }
