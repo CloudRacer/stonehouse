5f1debd84e891bafc2cedf452c2d0d3c5b74ba7a
weblogic <john@mcdonnell.org.uk>
2015-09-21 19:25:57 +0100

    Revert to a Spring Start parent in order to take advantage of the out-of-the-box package assembly and deployment.

--- application/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-JAR=application-1.0-SNAPSHOT.jar
-MAIN_CLASS=uk.org.mcdonnell.stonehouse.Application
-LIB_FOLDER=lib
-
-jar xf $JAR $LIB_FOLDER
-java -cp ./*;./$LIB_FOLDER/* $MAIN_CLASS
