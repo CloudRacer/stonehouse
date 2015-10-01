1e8bab30fa6f794ad7b83041c024d480cae23bcb
weblogic <john@mcdonnell.org.uk>
2015-10-01 14:59:56 +0100

    Defunct.

--- stonehouse.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mvn -B archetype:generate \
-  -DarchetypeGroupId=org.apache.maven.archetypes \
-  -DgroupId=uk.org.mcdonnell.stonehouse \
-  -DartifactId=plugins \
-  -Dname=Plugins
-
-read -p "Press [Enter] key to start continue..."
