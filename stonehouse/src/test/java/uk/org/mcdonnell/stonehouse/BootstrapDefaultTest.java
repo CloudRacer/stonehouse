1706bba446a89ac7085cbdb78a2aad2e1a0b40f6
weblogic <john@mcdonnell.org.uk>
2015-10-04 22:02:52 +0100

    Check that all plugins are loaded into the classpath.

--- stonehouse/src/test/java/uk/org/mcdonnell/stonehouse/BootstrapDefaultTest.java
+++ stonehouse/src/test/java/uk/org/mcdonnell/stonehouse/BootstrapDefaultTest.java
@@ -3,6 +3,8 @@ package uk.org.mcdonnell.stonehouse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
@@ -32,9 +34,37 @@ public class BootstrapDefaultTest {
 
             final List<File> files = FileHelper.getFileList(pluginFolder, ".*.jar");
             final Collection<File> missingPlugins = CollectionUtils.subtract(files, expectedPluginFilenames);
-            assertTrue(String.format("Plugin(s) %s were not loaded.", missingPlugins.toString()), missingPlugins.size() == 0);
+            assertTrue(String.format("Plugin(s) %s were not found.", missingPlugins.toString()), missingPlugins.size() == 0);
+
+            for (final File file : files) {
+                assertTrue(String.format("Plugin \"%s\" was not loaded into the classpath.", file.getAbsolutePath()), isJarInClasspath(file));
+            }
         } catch (final Exception e) {
             Assert.fail(e.getMessage());
+        }
+    }
+
+    public static boolean isJarInClasspath(File jarFile) {
+        final ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+
+        if (classLoader instanceof URLClassLoader) {
+            @SuppressWarnings("resource")
+            final URLClassLoader classLoader2 = (URLClassLoader) classLoader;
+            final URL[] urls = classLoader2.getURLs();
+
+            for (final URL url : urls) {
+                final File file = new File(url.getFile());
+
+                if (file.getPath().endsWith(jarFile.getName())) {
+                    System.out.println(jarFile + " exist");
+                    return true;
+                }
+            }
+
+            System.out.println(jarFile + " not exist");
+            return false;
+        } else {
+            return false;
         }
     }
 }
