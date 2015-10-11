3aa87ba1ec1fb8677811328ba501509924792c3f
weblogic <john@mcdonnell.org.uk>
2015-10-11 20:50:18 +0100

    Refactoring.

---
+++ utility/src/main/java/uk/org/mcdonnell/utility/common/Bootstrap.java
@@ -0,0 +1,31 @@
+package uk.org.mcdonnell.utility.common;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.MalformedURLException;
+
+/**
+ * Hello world!
+ *
+ */
+public final class Bootstrap {
+    private final File DEFAULT_FOLDER = new File("./plugins");
+    private final String[] DEFAULT_FILE_EXTENSION = new String[] { "jar" };
+
+    public Bootstrap() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, MalformedURLException, IOException {
+        new Bootstrap(DEFAULT_FOLDER, DEFAULT_FILE_EXTENSION);
+    }
+
+    public Bootstrap(final File folder) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, MalformedURLException, IOException {
+        new Bootstrap(folder, DEFAULT_FILE_EXTENSION);
+    }
+
+    private Bootstrap(final File folder, final String[] extensions) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, MalformedURLException, IOException {
+        uk.org.mcdonnell.utility.generic.ClasspathHelper.getInstance().addFolder(folder, extensions);
+    }
+
+    public static void start() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, MalformedURLException, IOException {
+        new Bootstrap();
+    }
+}
