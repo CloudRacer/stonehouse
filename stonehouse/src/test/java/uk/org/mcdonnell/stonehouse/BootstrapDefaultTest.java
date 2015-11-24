3cf28e09f2e82de12e811c5a1c49f75641277639
John McDonnell <jmcdonnell3@csc.com>
2015-11-24 01:27:59 +0000

    Separate the test data validation out into a separate @Before method.

--- stonehouse/src/test/java/uk/org/mcdonnell/stonehouse/BootstrapDefaultTest.java
+++ stonehouse/src/test/java/uk/org/mcdonnell/stonehouse/BootstrapDefaultTest.java
@@ -3,6 +3,9 @@ package uk.org.mcdonnell.stonehouse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.Arrays;
@@ -12,36 +15,38 @@ import java.util.Set;
 
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.io.FileUtils;
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import uk.org.mcdonnell.utility.common.Bootstrap;
 
 public class BootstrapDefaultTest {
 
+    final File pluginFolder = new File("./plugins");
+    Set<File> expectedPluginFiles;
+    Collection<File> actualPluginFiles;
+
+    @Before
+    public void ensureDefaultPluginFilesExist() {
+        expectedPluginFiles = new HashSet<File>(
+                Arrays.asList(
+                        new File(String.format("%s/%s", pluginFolder, "activemq-all-5.11.1.jar")),
+                        new File(String.format("%s/%s", pluginFolder, "plugin-1.0-SNAPSHOT.jar")),
+                        new File(String.format("%s/%s", pluginFolder, "provider-1.0-SNAPSHOT.jar"))));
+
+        actualPluginFiles = FileUtils.listFiles(pluginFolder, new String[] { "jar" }, true);
+
+        assertEqualCollections(expectedPluginFiles, actualPluginFiles);
+    }
+
     @Test
-    public void loadDefaultBootstrap() {
-        final File pluginFolder = new File("./plugins");
-        try {
-            // Load plugin JAR files.
-            Bootstrap.start();
+    public void loadDefaultBootstrap() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, MalformedURLException, IOException {
+        // Load plugin JAR files.
+        Bootstrap.start();
 
-            final Set<File> expectedPluginFilenames = new HashSet<File>(
-                    Arrays.asList(
-                            new File(String.format("%s/%s", pluginFolder, "activemq-all-5.11.1.jar")),
-                            new File(String.format("%s/%s", pluginFolder, "plugin-1.0-SNAPSHOT.jar")),
-                            new File(String.format("%s/%s", pluginFolder, "provider-1.0-SNAPSHOT.jar"))));
-
-            final Collection<File> files = FileUtils.listFiles(pluginFolder, new String[] { "jar" }, true);
-
-            assertEqualCollections(expectedPluginFilenames, files);
-
-            // Check that each file in the plugin folder is also in the Classpath.
-            for (final File file : files) {
-                assertTrue(String.format("Plugin \"%s\" was not loaded into the classpath.", file.getAbsolutePath()), isJarInClasspath(file));
-            }
-        } catch (final Exception e) {
-            Assert.fail(e.getMessage());
+        // Check that each file in the plugin folder is also in the Classpath.
+        for (final File file : actualPluginFiles) {
+            assertTrue(String.format("Plugin \"%s\" was not loaded into the classpath.", file.getAbsolutePath()), isJarInClasspath(file));
         }
     }
 
